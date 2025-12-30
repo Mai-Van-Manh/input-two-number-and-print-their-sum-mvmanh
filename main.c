7c9af2d8aa5ddf9415cbb7bfc380bb451823903e
Mai Van Manh <mvmanh@gmail.com>
2025-12-30 08:31:52 +0000

    second try

--- main.c
+++ main.c
@@ -3,8 +3,14 @@
 int main(void) {
     
     // ‘Prompt the user with ‘Enter x: ’ and ‘Enter y: ’ (without a newline) to read two integers; if both are ≥0, print their sum; otherwise, repeat the prompts once more and print the sum if both ≥0, or print ‘invalid’ if either is negative; all prompts and outputs must match exactly, including spacing and newlines.’
-    
-    printf("Hello");
+    int x, y;
+    printf("Enter x: ");
+    scanf("%d", &x);
+
+    printf("Enter y: ");
+    scanf("%d", &y);
+
+    printf("%d", x + y);
 
     return 0;
 }
